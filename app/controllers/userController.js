e8eae9ee2c35d61c70a4970ca738afdad4c7a04e
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-11-07 06:10:11 +0530

    update README and added boom in catch block

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,14 +1,17 @@
 const models = require('../models');
+const Boom = require('boom');
 const read =  (request, reply) => {
   models.user.findAll().then((result) => {
     reply({'Users': result});
-  });
+  })
+  .catch((err) => reply(Boom.badImplementation('Error creating user:', err)));
 };
 
 const create = (request, reply) => {
   models.user.create({username: request.payload.username}).then((result) => {
     reply({'Users': result});
-  });
+  })
+  .catch((err) => reply(Boom.badImplementation('Error reading users:', err)));;
 };
 
 module.exports = {read, create};
